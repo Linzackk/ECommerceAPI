67ea20b01a5a228d44736bec0d9baf0df39340b5
Isaac de Medeiros <linzackmedeiros@gmail.com>
2026-04-06 14:53:14 -0300

    feat adicao de Propriedades de Navegação nos Models

--- ECommerce/Models/Pedido.cs
+++ ECommerce/Models/Pedido.cs
@@ -4,6 +4,7 @@
     {
         public string Id { get; private set; }
         public string IdUsuario { get; private set; }
+        public Usuario Usuario { get; private set; }
         public decimal Total { get; private set; }
         public bool Finalizado { get; private set; }
     }
--- ECommerce/Models/PedidoItem.cs
+++ ECommerce/Models/PedidoItem.cs
@@ -3,7 +3,9 @@
     public class PedidoItem
     {
         public string IdPedido { get; private set; }
+        public Pedido Pedido { get; private set; }
         public string IdItem { get; private set; }
+        public Item Item { get; private set; }
         public int Quantidade { get; private set; }
         public string Id { get; private set; }
         public decimal ValorUnitario { get; private set; }

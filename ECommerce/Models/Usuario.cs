1b52e38baddcdbaa68fe3802937d7baaabeb8272
Isaac de Medeiros <linzackmedeiros@gmail.com>
2026-04-07 14:29:12 -0300

    feat Encapsulamento de Usuario no Models

--- ECommerce/Models/Usuario.cs
+++ ECommerce/Models/Usuario.cs
@@ -12,5 +12,38 @@
         public string CPF { get; private set; }
         public Login Login { get; private set; }
         public List<Pedido> Pedidos { get; private set; } = new();
+
+        public Usuario(string nome, string telefone, string rua, string cidade, string numero, string cep, string cpf)
+        {
+            if (string.IsNullOrEmpty(nome))
+                throw new ArgumentException("Nome não pode ser nulo ou vazio.");
+
+            if (string.IsNullOrEmpty(telefone))
+                throw new ArgumentException("Telefone não pode ser nulo ou vazio.");
+
+            if (string.IsNullOrEmpty(rua))
+                throw new ArgumentException("Rua não pode ser nula ou vazio.");
+
+            if (string.IsNullOrEmpty(cidade))
+                throw new ArgumentException("Cidade não pode ser nula ou vazio.");
+
+            if (string.IsNullOrEmpty(numero))
+                throw new ArgumentException("Numero não pode ser nulo ou vazio.");
+
+            if (string.IsNullOrEmpty(cep))
+                throw new ArgumentException("CEP não pode ser nulo ou vazio.");
+
+            if (string.IsNullOrEmpty(cpf))
+                throw new ArgumentException("CPF não pode ser nulo ou vazio.");
+
+            Id = Guid.NewGuid();
+            Nome = nome;
+            Telefone = telefone;
+            Rua = rua;
+            Cidade = cidade;
+            Numero = numero;
+            Cep = cep;
+            CPF = cpf;
+        }
     }
 }

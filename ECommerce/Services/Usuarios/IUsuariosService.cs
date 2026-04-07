2618b71392a6bbdfc7135db6d8952797b51de6ce
Isaac de Medeiros <linzackmedeiros@gmail.com>
2026-04-07 14:51:32 -0300

    feat Criacao da Interface do Service de Usuarios

---
+++ ECommerce/Services/Usuarios/IUsuariosService.cs
@@ -0,0 +1,13 @@
+﻿using ECommerce.Models;
+
+namespace ECommerce.Services.Usuarios
+{
+    public interface IUsuariosService
+    {
+        Task<UsuarioResponseDTO> CriarNovoUsuario(UsuarioCreateDTO novoUsuario);
+        Task<Usuario> ObterUsuarioPorId(Guid usuarioId);
+
+        Task RemoverUsuario(Guid usuarioId);
+        Task AtualizarUsuario(Guid usuarioId);
+    }
+}

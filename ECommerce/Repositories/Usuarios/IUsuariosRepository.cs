dcb3c0ad8a15c6f77e7ecaa3a7c9023f0fd9e8de
Isaac de Medeiros <linzackmedeiros@gmail.com>
2026-04-07 14:46:39 -0300

    feat Criacao do Usuario Repository em Repositories

---
+++ ECommerce/Repositories/Usuarios/IUsuariosRepository.cs
@@ -0,0 +1,12 @@
+﻿using ECommerce.Models;
+
+namespace ECommerce.Repositories.Usuarios
+{
+    public interface IUsuariosRepository
+    {
+        Task CriarUsuario(Usuario novoUsuario);
+        Task<Usuario?> ObterUsuarioPorId(Guid usuarioId);
+        Task AtualizarUsuario(Usuario usuarioAtualizado);
+        Task RemoverUsuario(Usuario usuario);
+    }
+}
---
+++ ECommerce/Repositories/Usuarios/UsuariosRepository.cs
@@ -0,0 +1,38 @@
+﻿using ECommerce.Data;
+using ECommerce.Models;
+
+namespace ECommerce.Repositories.Usuarios
+{
+    public class UsuariosRepository : IUsuariosRepository
+    {
+        private readonly AppDbContext _context;
+
+        public UsuariosRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task CriarUsuario(Usuario novoUsuario)
+        {
+            await _context.Usuarios.AddAsync(novoUsuario);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<Usuario?> ObterUsuarioPorId(Guid usuarioId)
+        {
+            var usuario = await _context.Usuarios.FindAsync(usuarioId);
+            return usuario;
+        }
+
+        public async Task RemoverUsuario(Usuario usuario)
+        {
+            _context.Usuarios.Remove(usuario);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task AtualizarUsuario(Usuario usuarioAtualizado)
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}

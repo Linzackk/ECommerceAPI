b13a0a5756cbbf8e8c3e654e7b7ba2ff3f0a3d95
Isaac de Medeiros <linzackmedeiros@gmail.com>
2026-04-08 15:38:43 -0300

    feat Teste de Procura com Erro 404 no endpoint de Usuarios

---
+++ ECommerce.Tests/Usuarios/UsuarioEndpointsTest.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Tests.Usuarios
+{
+    public class UsuarioEndpointsTest : IClassFixture<CustomWebApplicationFactory>
+    {
+        private readonly string _url = "api/Usuarios";
+        private readonly HttpClient _client;
+
+        public UsuarioEndpointsTest(CustomWebApplicationFactory factory)
+        {
+            _client = factory.CreateClient();
+        }
+
+        [Fact]
+        public async Task Deve_ProcurarUsuarioInexistente_Retornar404()
+        {
+            var id = Guid.NewGuid().ToString();
+            var response = await _client.GetAsync($"{_url}/{id}");
+            Assert.NotNull(response);
+            Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
+        }
+    }
+}

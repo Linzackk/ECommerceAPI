9bc59357047442be1dd51e6ae3fbcbed34aba411
Isaac de Medeiros <linzackmedeiros@gmail.com>
2026-04-08 15:38:17 -0300

    feat  Criação de Factory Customizada para isolamento de bancos em teste.

---
+++ ECommerce.Tests/CustomWebApplicationFactory.cs
@@ -0,0 +1,42 @@
+﻿using ECommerce.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Tests
+{
+    public class CustomWebApplicationFactory : WebApplicationFactory<Program>
+    {
+        private readonly string _dbName = Guid.NewGuid().ToString();
+
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.UseEnvironment("Testing");
+
+            builder.ConfigureServices(services =>
+            {
+                var descriptors = services
+                    .Where(d =>
+                        d.ServiceType == typeof(DbContextOptions<AppDbContext>) ||
+                        d.ServiceType == typeof(AppDbContext))
+                    .ToList();
+
+                foreach (var descriptor in descriptors)
+                {
+                    services.Remove(descriptor);
+                }
+                services.AddDbContext<AppDbContext>(options =>
+                {
+                    options.UseInMemoryDatabase(_dbName);
+                });
+            });
+        }
+    }
+}

8cda7a1ad04b2f865e6f09c16e1f7b578c2c3bb5
Isaac de Medeiros <linzackmedeiros@gmail.com>
2026-04-10 13:31:59 -0300

    feat Adição do Repository com interface de Login

---
+++ ECommerce/Repositories/Logins/ILoginRepository.cs
@@ -0,0 +1,11 @@
+﻿using ECommerce.Models;
+
+namespace ECommerce.Repositories.Logins
+{
+    public interface ILoginRepository
+    {
+        Task<Login?> ObterPorEmail(string email);
+        Task CriarLogin(Login novoLogin);
+        Task RemoverLogin(Login login);
+    }
+}
---
+++ ECommerce/Repositories/Logins/LoginRepository.cs
@@ -0,0 +1,34 @@
+﻿using ECommerce.Data;
+using ECommerce.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ECommerce.Repositories.Logins
+{
+    public class LoginRepository : ILoginRepository
+    {
+        private readonly AppDbContext _context;
+
+        public LoginRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task CriarLogin(Login novoLogin)
+        {
+            await _context.Login.AddAsync(novoLogin);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<Login?> ObterPorEmail(string email)
+        {
+            var login = await _context.Login.FirstOrDefaultAsync(login => login.Email == email);
+            return login;
+        }
+
+        public async Task RemoverLogin(Login login)
+        {
+            _context.Login.Remove(login);
+            await _context.SaveChangesAsync();
+        }
+    }
+}

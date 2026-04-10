fe05cce4012bb1d9d4be4dee202e12bb9fb843c7
Isaac de Medeiros <linzackmedeiros@gmail.com>
2026-04-10 13:45:41 -0300

    feat Adição do IDUsuario no DTO de criação de Login

--- ECommerce/DTOs/Login/LoginCreateDTO.cs
+++ ECommerce/DTOs/Login/LoginCreateDTO.cs
@@ -2,7 +2,8 @@
 {
     public class LoginCreateDTO
     {
+        public Guid IdUsuario { get; set; }
         public string Email { get; set; }
-        public string SenhaHash { get; set; }
+        public string Senha { get; set; }
     }
 }

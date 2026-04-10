3f8a7488148a3167d979cf84caf0d62d104dc7c2
Isaac de Medeiros <linzackmedeiros@gmail.com>
2026-04-10 13:51:52 -0300

    feat Service de Login feito com sucesso

---
+++ ECommerce/Services/Logins/LoginService.cs
@@ -0,0 +1,54 @@
+﻿using ECommerce.DTOs.Login;
+using ECommerce.Exceptions;
+using ECommerce.Models;
+using ECommerce.Repositories.Logins;
+
+namespace ECommerce.Services.Logins
+{
+    public class LoginService : ILoginService
+    {
+        private readonly ILoginRepository _repository;
+
+        public LoginService(ILoginRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<string> FazerLogin(LoginEntrarDTO credenciais)
+        {
+            var login = await ProcurarLoginPorEmail(credenciais.Email);
+
+            if (!BCrypt.Net.BCrypt.Verify(credenciais.Senha, login.Senha))
+                throw new LoginCredenciaisInvalidasException();
+
+            var token = "Login feito com sucesso.";
+            return token;
+        }
+
+        private async Task<Login> ProcurarLoginPorEmail(string email)
+        {
+            var login = await _repository.ObterPorEmail(email);
+            if (login == null)
+                throw new LoginCredenciaisInvalidasException();
+            return login;
+        }
+
+        private Login CriarLoginPorDTO(LoginCreateDTO novoLogin)
+        {
+            var senhaHash = BCrypt.Net.BCrypt.HashPassword(novoLogin.Senha);
+            return new Login(novoLogin.Email, senhaHash, novoLogin.IdUsuario);
+        }
+
+        public async Task CriarLogin(LoginCreateDTO novoLogin)
+        {
+            var login = CriarLoginPorDTO(novoLogin);
+            await _repository.CriarLogin(login);
+        }
+
+        public async Task DeletarLogin(string email)
+        {
+            var login = await ProcurarLoginPorEmail(email);
+            await _repository.RemoverLogin(login);
+        }
+    }
+}

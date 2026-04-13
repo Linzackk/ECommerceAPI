88a127fbe10c07a94964305d3e9156d17650f699
Isaac de Medeiros <linzackmedeiros@gmail.com>
2026-04-13 13:54:01 -0300

    feat Teste de Criação com Login inválido

--- ECommerce.Tests/Logins/LoginServiceTest.cs
+++ ECommerce.Tests/Logins/LoginServiceTest.cs
@@ -1,4 +1,6 @@
-﻿using ECommerce.Models;
+﻿using ECommerce.DTOs.Login;
+using ECommerce.Exceptions;
+using ECommerce.Models;
 using ECommerce.Repositories.Logins;
 using ECommerce.Services.Logins;
 using Moq;
@@ -15,14 +17,29 @@ namespace ECommerce.Tests.Logins
         static string senhaTeste = "senhaSenha";
         static string emailTeste = "email@email.com";
 
-        private static Login CriarNovoLogin()
+        private static Login CriarNovoLoginValido()
         {
             return new Login(emailTeste, senhaTeste, Guid.NewGuid());
         }
+
+        private static LoginCreateDTO CriarNovoLoginDTOValido()
+        {
+            var login = new LoginCreateDTO();
+            login.Email = emailTeste;
+            login.Senha = senhaTeste;
+            login.IdUsuario = Guid.NewGuid();
+            return login;
+        }
+
+        private static LoginCreateDTO CriarNovoLoginDTOInvalido()
+        {
+            return new LoginCreateDTO();
+        }
+
         [Fact]
         public async Task CriarLogin_DeveCriarLogin_BuscarLoginCriado()
         {
-            var login = CriarNovoLogin();
+            var login = CriarNovoLoginValido();
 
             var mock = new Mock<ILoginRepository>();
 
@@ -36,6 +53,18 @@ namespace ECommerce.Tests.Logins
             Assert.NotNull(resultado);
             Assert.Equal(login.Email, resultado.Email);
             Assert.Equal(login.IdUsuario, resultado.IdUsuario);
+        }
+
+        [Fact]
+        public async Task CriarLogin_DeveLancarExcessao_DadosInvalidosParaCriar()
+        {
+            var login = CriarNovoLoginDTOInvalido();
+
+            var mock = new Mock<ILoginRepository>();
+
+            var service = new LoginService(mock.Object);
+
+            await Assert.ThrowsAsync<ParametroInvalidoException>(() => service.CriarLogin(login));
         }
     }
 }

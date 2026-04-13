e4a5db2b69e0049fef3fb6e61ab9c496e274831e
Isaac de Medeiros <linzackmedeiros@gmail.com>
2026-04-13 14:58:39 -0300

    feat Criacao de DTOs de Itens

---
+++ ECommerce/DTOs/Itens/ItemResponseDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace ECommerce.DTOs.Itens
+{
+    public class ItemResponseDTO
+    {
+        public Guid Id { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public int Estoque { get; set; }
+        public decimal Preco { get; set; }
+        public DateOnly DataCriacao { get; set; }
+    }
+}
---
+++ ECommerce/DTOs/Itens/ItemUpdateDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace ECommerce.DTOs.Itens
+{
+    public class ItemUpdateDTO
+    {
+        public string? Nome { get; set; }
+        public string? Descricao { get; set; }
+        public int? Estoque { get; set; }
+        public decimal? Preco { get; set; }
+    }
+}

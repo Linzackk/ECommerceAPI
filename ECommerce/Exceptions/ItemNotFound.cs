7341de6e178cd708148b57e2a38edbae1906f6ba
Isaac de Medeiros <linzackmedeiros@gmail.com>
2026-04-13 14:55:24 -0300

    feat Criação do Repository de Itens

---
+++ ECommerce/Exceptions/ItemNotFound.cs
@@ -0,0 +1,8 @@
+﻿namespace ECommerce.Exceptions
+{
+    public class ItemNotFound : NotFoundException
+    {
+        public ItemNotFound()
+            : base("Item não encontrado") { }
+    }
+}
---
+++ ECommerce/Repositories/Itens/ItemRepository.cs
@@ -0,0 +1,43 @@
+﻿using ECommerce.Data;
+using ECommerce.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ECommerce.Repositories.Itens
+{
+    public class ItemRepository : IItemRepository
+    {
+        private readonly AppDbContext _context;
+        public ItemRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task CriarItem(Item novoItem)
+        {
+            await _context.Itens.AddAsync(novoItem);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<Item?> ObterItemPorId(Guid itemId)
+        {
+            var item = await _context.Itens.FirstOrDefaultAsync(x => x.Id == itemId);
+            return item;
+        }
+
+        public async Task  AtualizarItem(Item itemAtualizado)
+        {
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task RemoverItem(Item item)
+        {
+            _context.Itens.Remove(item);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<IReadOnlyCollection<Item>> ObterTodos()
+        {
+            var itens = await _context.Itens.ToListAsync();
+            return itens;
+        }
+    }
+}

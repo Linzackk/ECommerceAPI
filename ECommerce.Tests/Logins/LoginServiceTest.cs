ac186ad0cdc9540fdbfadc2eac9c047e3ba5c9ea
Isaac de Medeiros <linzackmedeiros@gmail.com>
2026-04-13 14:18:44 -0300

    feat Teste de Login com Resposta de Token Válido

--- ECommerce.Tests/Logins/LoginServiceTest.cs
+++ ECommerce.Tests/Logins/LoginServiceTest.cs
@@ -15,12 +15,13 @@ namespace ECommerce.Tests.Logins
     public class LoginServiceTest
     {
         static string senhaTeste = "senhaSenha";
+        static string senhaHash = BCrypt.Net.BCrypt.HashPassword(senhaTeste);
         static string emailTeste = "email@email.com";
         static Guid usuarioIdTeste = Guid.NewGuid();
 
         private static Login CriarNovoLoginValido()
         {
-            return new Login(emailTeste, senhaTeste, usuarioIdTeste);
+            return new Login(emailTeste, senhaHash, usuarioIdTeste);
         }
 
         private static LoginCreateDTO CriarNovoLoginDTOValido()
@@ -103,6 +104,30 @@ namespace ECommerce.Tests.Logins
             var service = new LoginService(mock.Object);
 
             await Assert.ThrowsAsync<LoginCredenciaisInvalidasException>(() => service.ProcurarLoginPorEmail(emailTeste));
+        }
+
+        [Fact]
+        public async Task FazerLogin_DeveCriarLogin_RetornarTokenValido()
+        {
+            var loginResponse = CriarNovoLoginValido();
+            var loginCreate = CriarNovoLoginDTOValido();
+            var loginEntrar = new LoginEntrarDTO();
+            loginEntrar.Email = emailTeste;
+            loginEntrar.Senha = senhaTeste;
+
+            var mock = new Mock<ILoginRepository>();
+
+            mock.Setup(x => x.ObterPorEmail(emailTeste))
+                .ReturnsAsync(loginResponse);
+
+            var service = new LoginService(mock.Object);
+
+            await service.CriarLogin(loginCreate);
+
+            var response = await service.FazerLogin(loginEntrar);
+
+            Assert.NotNull(response);
+            Assert.Equal("Login feito com sucesso.", response);
         }
     }
 }

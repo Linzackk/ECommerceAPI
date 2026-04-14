f8e78e42de1489ff41a42ff5676e8ae10b9202ca
Isaac de Medeiros <linzackmedeiros@gmail.com>
2026-04-14 14:50:31 -0300

    feat Adição de repository para Pedidos

---
+++ ECommerce/Repositories/Pedidos/IPedidosRepository.cs
@@ -0,0 +1,13 @@
+﻿using ECommerce.Models;
+
+namespace ECommerce.Repositories.Pedidos
+{
+    public interface IPedidosRepository
+    {
+        Task CriarNovoPedido(Pedido novoPedido);
+        Task<Pedido?> ObterPedidoPorId(Guid pedidoId);
+        Task<IReadOnlyCollection<Pedido>> ObterPedidosPorIdUsuario(Guid idUsuario);
+        Task AtualizarPedido(Pedido pedido);
+        Task RemoverPedido(Pedido pedido);
+    }
+}
---
+++ ECommerce/Repositories/Pedidos/PedidosRepository.cs
@@ -0,0 +1,44 @@
+﻿using ECommerce.Data;
+using ECommerce.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ECommerce.Repositories.Pedidos
+{
+    public class PedidosRepository : IPedidosRepository
+    {
+        private readonly AppDbContext _context;
+        public PedidosRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task CriarNovoPedido(Pedido novoPedido)
+        {
+            await _context.Pedidos.AddAsync(novoPedido);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<Pedido?> ObterPedidoPorId(Guid pedidoId)
+        {
+            var pedido = await _context.Pedidos.FirstOrDefaultAsync(p => p.Id.Equals(pedidoId));
+            return pedido;
+        }
+
+        public async Task<IReadOnlyCollection<Pedido>> ObterPedidosPorIdUsuario(Guid idUsuario)
+        {
+            var pedidos = await _context.Pedidos.Where(p => p.IdUsuario.Equals(idUsuario)).ToListAsync();
+            return pedidos;
+        }
+
+        public async Task RemoverPedido(Pedido pedido)
+        {
+            _context.Pedidos.Remove(pedido);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task AtualizarPedido(Pedido pedido)
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}

841b335e4cadb9276d23423017b89f7d8ca45eab
Isaac de Medeiros <linzackmedeiros@gmail.com>
2026-04-14 14:27:33 -0300

    feat Encapsulação e Método de adicionar item ao Pedido.

--- ECommerce/Models/Pedido.cs
+++ ECommerce/Models/Pedido.cs
@@ -1,12 +1,36 @@
-﻿namespace ECommerce.Models
+﻿using ECommerce.Exceptions;
+
+namespace ECommerce.Models
 {
     public class Pedido
     {
         public Guid Id { get; private set; }
         public Guid IdUsuario { get; private set; }
-        public Usuario Usuario { get; private set; }
         public bool Finalizado { get; private set; }
-
         public List<PedidoItem> Itens { get; private set; } = new();
+
+        public Pedido(Guid idUsuario)
+        {
+            if (idUsuario.Equals(Guid.Empty))
+                throw new ParametroInvalidoException("ID do usuário não pode estar vazio.");
+
+            Id = Guid.NewGuid();
+            IdUsuario = idUsuario;
+            Finalizado = false;
+        }
+
+        public void AdicionarNovoItem(Item novoItem, int quantidade)
+        {
+            if (Finalizado)
+                throw new ParametroInvalidoException("Não é possivel alterar o pedido, ele já foi fechado.");
+
+            var itemExistente = Itens.FirstOrDefault(pi => pi.IdItem == novoItem.Id);
+
+            if (itemExistente != null)
+                throw new ParametroInvalidoException("Item já adicionado ao Pedido.");
+
+            var pedidoItem = new PedidoItem(Id, novoItem.Id, quantidade, novoItem.Preco);
+            Itens.Add(pedidoItem);
+        }
     }
 }

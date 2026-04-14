668d1a0625eefe507a1ec7de8d67b6d348772894
Isaac de Medeiros <linzackmedeiros@gmail.com>
2026-04-14 15:07:40 -0300

    feat DTOs para  transmissão de informações

---
+++ ECommerce/DTOs/Pedidos/PedidoCreateDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace ECommerce.DTOs.Pedidos
+{
+    public class PedidoCreateDTO
+    {
+        public Guid IdUsuario { get; set; }
+        public IReadOnlyCollection<Guid>? IdsItens { get; set; }
+
+    }
+}

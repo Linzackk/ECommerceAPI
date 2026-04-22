e43112ea86ea3a1156c336e4043eefbe3e40d3c3
Isaac de Medeiros <linzackmedeiros@gmail.com>
2026-04-22 14:44:45 -0300

    feat Método de Adição de PedidoItem no contexto em PedidoItemHelper

---
+++ ECommerce.Tests/Helpers/PedidoItemHelper.cs
@@ -0,0 +1,36 @@
+﻿using ECommerce.DTOs.Pedidos;
+using ECommerce.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Tests.Helpers
+{
+    public class PedidoItemHelper
+    {
+        private readonly HttpClient _client;
+        public PedidoItemHelper(HttpClient client)
+        {
+            _client = client;
+        }
+
+        public PedidoItemCreateDTO CriarPedidoItemValido(Guid itemId, int quantidade)
+        {
+            return new PedidoItemCreateDTO()
+            {
+                ItemId = itemId,
+                Quantidade = quantidade
+            };
+        }
+
+        public async Task CriarPedidoItem_NoContexto(PedidoItemCreateDTO novoPedidoItem, string url)
+        {
+            var postResponse = await _client.PostAsJsonAsync(url, novoPedidoItem);
+            if (!postResponse.IsSuccessStatusCode)
+                throw new Exception("Não foi possível adicionar o PedidoItem no Contexto.");
+        }
+    }
+}

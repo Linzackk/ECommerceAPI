eb9a20536d344a63e2c0b0ed6d03239317930e7b
Isaac de Medeiros <linzackmedeiros@gmail.com>
2026-04-23 15:03:15 -0300

    feat Policy e Handler para Permitir apenas dono do pedido e admin alterar

---
+++ ECommerce/Authorization/Handlers/OrderOwnerOrAdminHandler.cs
@@ -0,0 +1,44 @@
+﻿using ECommerce.Authorization.Policies;
+using ECommerce.Repositories.Pedidos;
+using Microsoft.AspNetCore.Authorization;
+using System.IdentityModel.Tokens.Jwt;
+
+namespace ECommerce.Authorization.Handlers
+{
+    public class OrderOwnerOrAdminHandler : AuthorizationHandler<OrderOwnerOrAdminRequirement>
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        private readonly IPedidosRepository _pedidoRepository;
+
+        public OrderOwnerOrAdminHandler(IHttpContextAccessor httpContextAccessor, IPedidosRepository pedidoRepository)
+        {
+            _httpContextAccessor = httpContextAccessor;
+            _pedidoRepository = pedidoRepository;
+        }
+
+        protected override async Task HandleRequirementAsync(AuthorizationHandlerContext context, OrderOwnerOrAdminRequirement requirement)
+        {
+            var httpContext = _httpContextAccessor.HttpContext;
+
+            var isAdmin = context.User.FindFirst("isAdmin")?.Value == "true";
+            if (isAdmin)
+            {
+                context.Succeed(requirement);
+                return;
+            }
+
+            var userIdClaim = context.User.FindFirst(JwtRegisteredClaimNames.Sub)?.Value;
+            if (userIdClaim == null)
+                return;
+
+            var routeValue = httpContext?.GetRouteValue("pedidoId")?.ToString();
+            if (!Guid.TryParse(routeValue, out var pedidoId)) return;
+
+            var pedido = await _pedidoRepository.ObterPedidoPorId(pedidoId);
+            if (pedido == null) return;
+
+            if (userIdClaim != null && pedido.IdUsuario == Guid.Parse(userIdClaim))
+                context.Succeed(requirement);
+        }
+    }
+}
---
+++ ECommerce/Authorization/Policies/OrderOwnerOrAdminRequirement.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Authorization;
+
+namespace ECommerce.Authorization.Policies
+{
+    public class OrderOwnerOrAdminRequirement : IAuthorizationRequirement
+    { 
+    }
+}

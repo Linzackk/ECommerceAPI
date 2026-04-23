60c0a660129dea4fcd9194cdbe511740e3a273a1
Isaac de Medeiros <linzackmedeiros@gmail.com>
2026-04-23 14:49:16 -0300

    feat Policy e Handler para Apenas dono da conta ou admin poderem mexer

---
+++ ECommerce/Authorization/Handlers/AccountOwnerOrAdminHandler.cs
@@ -0,0 +1,37 @@
+﻿using ECommerce.Authorization.Policies;
+using Microsoft.AspNetCore.Authorization;
+using System.IdentityModel.Tokens.Jwt;
+
+namespace ECommerce.Authorization.Handlers
+{
+    public class AccountOwnerOrAdminHandler : AuthorizationHandler<AccountOwnerOrAdminRequirement>
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public AccountOwnerOrAdminHandler(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, AccountOwnerOrAdminRequirement requirement)
+        {
+            var httpContext = _httpContextAccessor.HttpContext;
+
+            var isAdmin = context.User.FindFirst("isAdmin")?.Value == "true";
+            if (isAdmin)
+            {
+                context.Succeed(requirement);
+                return Task.CompletedTask;
+            }
+
+            var userIdClaim = context.User.FindFirst(JwtRegisteredClaimNames.Sub)?.Value;
+
+            var routeId = httpContext?.GetRouteValue("id")?.ToString();
+
+            if (userIdClaim != null && userIdClaim == routeId)
+                context.Succeed(requirement);
+
+            return Task.CompletedTask;
+        }
+    }
+}

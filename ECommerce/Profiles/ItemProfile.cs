8e817e6edbc546bfe6ee3e5a239c0aa4281f25ff
Isaac de Medeiros <linzackmedeiros@gmail.com>
2026-04-27 15:43:47 -0300

    feat Profile de Mapper para Itens

---
+++ ECommerce/Profiles/ItemProfile.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using ECommerce.DTOs.Itens;
+using ECommerce.Models;
+
+namespace ECommerce.Profiles
+{
+    public class ItemProfile : Profile
+    {
+        public ItemProfile() 
+        {
+            CreateMap<Item, ItemResponseDTO>();
+            CreateMap<ItemCreateDTO, Item>()
+                .ConstructUsing(src => new Item(
+                    src.Nome,
+                    src.Descricao,
+                    src.Estoque,
+                    src.Preco
+                ));
+        }
+    }
+}

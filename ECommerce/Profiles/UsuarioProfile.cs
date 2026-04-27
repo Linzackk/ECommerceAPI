ef689c95856c6634f787ff348cf7d1bf8e4c1b02
Isaac de Medeiros <linzackmedeiros@gmail.com>
2026-04-27 14:59:27 -0300

    feat Adição do AutoMapper para usuário

---
+++ ECommerce/Profiles/UsuarioProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using ECommerce.DTOs.Usuarios;
+using ECommerce.Models;
+
+namespace ECommerce.Profiles
+{
+    public class UsuarioProfile : Profile
+    {
+        public UsuarioProfile()
+        {
+            CreateMap<Usuario, UsuarioResponseDTO>();
+            CreateMap<UsuarioCreateDTO, Usuario>();
+        }
+    }
+}
